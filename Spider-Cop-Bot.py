1a50aa528399264fbc43f571bde0575bd2799223
dhruva <dhruva.rajkumar@uwaterloo.ca>
2024-12-28 20:28:14 -0500

    added toxic leaderboard command, !leaderboard and added a count storage of all toxic messages by server and then by user

--- Spider-Cop-Bot.py
+++ Spider-Cop-Bot.py
@@ -5,6 +5,7 @@ from googleapiclient import discovery
 import json
 import config
 import random
+from discord.ext import commands
 
 # defined in a file called config.py (not pushed to github because that would not be wise :D)
 API_KEY = config.API_KEY
@@ -15,7 +16,7 @@ answers = config.responses
 intents = discord.Intents.default()
 intents.message_content = True
 
-client = discord.Client(intents=intents)
+client = commands.Bot(command_prefix="!", intents=intents)
 
 @client.event
 async def on_ready():
@@ -34,18 +35,23 @@ async def on_message(message):
     # prevents analysis on links 
     if 'https://' in message.content:
         return
-        
-    score = toxicity_analysis(message.content)
-    print(score)
+    
+    # runs the api if the leaderboard command is not in the message (could use starts with as well)
+    if '!leaderboard' not in message.content:
+        score = toxicity_analysis(message.content)
+        print(score)
 
-    # This value in this conditional is arbitrary 
-    if score > 0.90:
-        # the array answers, is defined in config.py and contains cdn's of images and responses
-        # the random library picks any of these messages at random to send to the user
-        await message.reply(random.choice(answers))
+        # This value in this conditional is arbitrary 
+        if score > 0.88:
+            # the array answers, is defined in config.py and contains cdn's of images and responses
+            # the random library picks any of these messages at random to send to the user
+            await message.reply(random.choice(answers))
+            update_toxicity_count(message.guild.id, message.author.name)
 
-    # TODO adding a tier list of some sort (tracks the most toxic messages :) )
+    await client.process_commands(message)  # Allow commands to be processed
 
+# Connects to the perspective API to peform a check on if the message is toxic
+# Uses an NLP to accomplish this
 def toxicity_analysis(message: str) -> int:
     clienttwo = discovery.build(
     "commentanalyzer",
@@ -62,8 +68,65 @@ def toxicity_analysis(message: str) -> int:
     response = clienttwo.comments().analyze(body=analyze_request).execute()
     response = json.dumps(response, indent=2)
     response = json.loads(response)
+    # We grab the probability
     return response["attributeScores"]["TOXICITY"]["summaryScore"]["value"]
-    
 
-client.run(DISCORD_TOKEN)
+
+# Whenever the perspective call deems a message toxic, the count is then updated
+# JSON file because scalability is not required and this is relatively simple to use
+def update_toxicity_count(guild_id, user_id):
+    """Update the count of toxic messages for a user in a JSON file."""
+    try:
+        with open('count.json', 'r') as f:
+            data = json.load(f)
+    except (FileNotFoundError, json.JSONDecodeError):
+        data = {}
+
+    # Initialize guild and user data if not present
+    if str(guild_id) not in data:
+        data[str(guild_id)] = {}
+    if user_id not in data[str(guild_id)]:
+        data[str(guild_id)][user_id] = {"count": 0}
+
+    # Increment the toxic message count
+    data[str(guild_id)][str(user_id)]["count"] += 1
+
+    # Save the updated data back to the JSON file
+    with open('count.json', 'w') as f:
+        json.dump(data, f, indent=4)
+
+
+# Leaderboard command called with !leaderboard
+@client.command()
+async def leaderboard(ctx, x=10):
+    with open('count.json', 'r') as f:
+        users = json.load(f)
+
+    guild_id = str(ctx.guild.id)
+
+    if guild_id not in users:
+        await ctx.send("No data available for this server.")
+        return
+
+    leaderboard = []
+
+    for user, data in users[guild_id].items():
+        leaderboard.append((user, data['count']))
+
+    # Sort the leaderboard by count in descending order
+    leaderboard.sort(key=lambda item: item[1], reverse=True)
+
+    em = discord.Embed(
+        title=f'Top {x} most toxic members of {ctx.guild.name}',
+        description='hall of shame..'
+    )
+
+    for index, (user, count) in enumerate(leaderboard[:x], start=1):
+        # Might add a tier break system as well using the value section
+        em.add_field(name=f'**#{index}**: **{user}**', value="", inline=False)
+
+    await ctx.send(embed=em)
+
+if __name__=="__main__":
+    client.run(DISCORD_TOKEN)
 
