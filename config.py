2973ffb4724c1bad737c22340526b29ae356ab6c
dhruva <dhruva.rajkumar@uwaterloo.ca>
2024-12-28 21:15:59 -0500

    Adding visibility to basic config file

---
+++ config.py
@@ -0,0 +1,15 @@
+API_KEY=''
+DISCORD_TOKEN=''
+responses = ["You should read this https://cdn.discordapp.com/attachments/1322631350338982052/1322664400833745016/book.jpg?ex=6771b2b7&is=67706137&hm=d82b32d0761962a98c2e014d588e8e9147aa844fa1fc6ab7bc6b8be58952105d&", 
+             "https://cdn.discordapp.com/attachments/1322631350338982052/1322666558350299248/upset-gamer-lost-ranked-game-dubsnatch.webp?ex=6771b4b9&is=67706339&hm=8a781e464357d1297072adda849047226041c5c07c7943030d275591510129d7&", 
+             "https://cdn.discordapp.com/attachments/1322631350338982052/1322666932461109248/3x.webp?ex=6771b512&is=67706392&hm=9d403a92bedf2673f052e4a8a540f42d74ef342bb15b9568fd014d2f70ee0bf4&",
+             "https://cdn.discordapp.com/attachments/1322631350338982052/1322668615375257650/71jd87zdReS.jpg?ex=6771b6a3&is=67706523&hm=3d3c3822cdad48990f1f28bc60b883df71dbf6a0eb0725b4199378af02699737&",
+             "https://cdn.discordapp.com/attachments/1322631350338982052/1322668705674694666/true.png?ex=6771b6b9&is=67706539&hm=e3303223395eb4df18d2f83b941a92c08d5181575d052bd8ac569400c400c25f&",
+             "https://cdn.discordapp.com/attachments/1322631350338982052/1322668835916087327/L.jpg?ex=6771b6d8&is=67706558&hm=f5d09f027cfed1c3fdb5410bbf548dcae57c968a0be562e644314535a0aebb3e&",
+             "https://cdn.discordapp.com/attachments/1322631350338982052/1322679735003779072/image.png?ex=6771c0ff&is=67706f7f&hm=8894a0f5ea3b04a38e19b3865ab0b52dcb388c4e26efe24d5a8382e8676837a2&",
+             "https://cdn.discordapp.com/attachments/1322631350338982052/1322680228119711844/200w.gif?ex=6771c174&is=67706ff4&hm=0d3addd2a3f559238a0b8cf724a8a78320cca1d23a5e99b18cfde0910850317c&",
+             "https://open.spotify.com/track/6I9VzXrHxO9rA9A5euc8Ak?si=efd5363248d1471e",
+             "https://tenor.com/view/your-toxic-toxic-oprah-gif-26023505",
+             "https://tenor.com/view/bitrix24-bitrix24fun-toxic-why-so-toxic-so-mean-gif-20152975"
+             ]
+
